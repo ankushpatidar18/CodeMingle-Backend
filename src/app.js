24690452fbefed6228ba78dbbf2df7fa90cd39fe
ankushpatidar18 <ankushpatidar18@gmail.com>
2024-11-03 18:04:56 +0530

    data sanitization(not fully)

--- src/app.js
+++ src/app.js
@@ -3,86 +3,98 @@
 //install express
 
 //requiring database so that first db connect then server listen
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
 
 //for process.env to secure cluster uri
-require('dotenv').config();
+require("dotenv").config();
 
-const User = require("./models/user")
-
+const User = require("./models/user");
 
 //creating webserver through express.js
 const express = require("express");
 const app = express();
 
 //middleware for read the json and convert into javascript object
-app.use(express.json())
+app.use(express.json());
 
-
-app.patch("/user",async (req,res)=>{
-    const data = req.body;
-    const id = req.body.id;
-    try{
-          await User.findByIdAndUpdate(id,data,{ runValidators : true })
-          res.send("Updated Successfully")
-    }catch(err){
-        res.send("problem in updating" + err.message)
+app.patch("/user", async (req, res) => {
+    try {
+      const NOT_ALLOWED_UPDATES = [
+        "emailId",
+        "firstName",
+        "lastName",
+        "password",
+        "age",
+        "gender",
+      ];
+      const keys_arr = Object.keys(req.body);
+      const not_allowed = keys_arr.some((ele) => NOT_ALLOWED_UPDATES.includes(ele));
+      const data = req.body;
+      const id = req.body.id;
+  
+      if (!id) {
+        return res.status(400).json({ message: "User  ID is required" });
+      }
+      if (!not_allowed) {
+        await User.findByIdAndUpdate(id, data, { runValidators: true });
+        return res.status(200).json({ message: "Updated Successfully" });
+      } else {
+        return res.status(400).json({ message: "These attributes cannot be updated" });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: "Problem in updating: " + err.message });
     }
-})
-
-app.delete("/user",async (req,res)=>{
+  });
+  
+  app.delete("/user", async (req, res) => {
     const userId = req.body.id;
-    try{
-          await User.findByIdAndDelete(userId)
-          res.send("Deleted Successfully")
-    }catch(err){
-          res.status(400).send("something went wrong");
+    if (!userId) {
+      return res.status(400).json({ message: "User  ID is required" });
     }
-})
-
-
+    
+    try {
+      const deletedUser  = await User.findByIdAndDelete(userId);
+      if (!deletedUser ) {
+        return res.status(404).json({ message: "User  not found" });
+      }
+      res.json({ message: "Deleted Successfully" });
+    } catch (err) {
+      res.status(500).json({ message: "Error deleting user: " + err.message });
+    }
+  });
 //middleware for getting all the user data
-app.get("/feed",async (req,res)=>{
-    
-   try{
+app.get("/feed", async (req, res) => {
+  try {
     const users = await User.find();
-    if(users.length===0)
-    {
-        res.status(400).send("No Registered User");
+    if (users.length === 0) {
+      res.status(400).send("No Registered User");
+    } else {
+      res.send(users);
     }
-    else{
-        res.send(users);
-    }
-    
-   }catch(err){
+  } catch (err) {
     res.status(400).send("something went wrong");
-   }
-})
-
-
+  }
+});
 
 //middleware for adding user
-app.post("/signup",async (req,res)=>{
-   
-    //creating a new instance of model
-     const user = new User(req.body)
-     
-     try{
-     //returns a promise
-     await user.save();
-     res.send("User created successfully")
-     }catch(err) {
-         res.status(400).send("User is not Created because of ",err.message)
-     }
-   
-})
+app.post("/signup", async (req, res) => {
+  //creating a new instance of model
+  const user = new User(req.body);
 
+  try {
+    //returns a promise
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    res.status(400).send("User is not Created because of " + err.message);
+  }
+});
 
-connectDB().then(()=>{
-    console.log("database connected")
-    app.listen(3000,()=>{
-        console.log('server is listening on port number 3000')
+connectDB()
+  .then(() => {
+    console.log("database connected");
+    app.listen(3000, () => {
+      console.log("server is listening on port number 3000");
     });
- }).catch((err)=> console.log(err))
-
-
+  })
+  .catch((err) => console.log(err));
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const npm_validator = require("validator")
 
 //1.creating user attributes
 //2.adding validation
@@ -17,11 +18,23 @@ const userSchema = new mongoose.Schema({
         lowercase : true,
         required : true,
         unique : true,
-        trim :true
+        trim :true,
+        validate: {
+            //you can also use npm validator
+            validator: function (value) {
+              return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value); // Simple regex for email format
+            },
+            message: props => `${props.value} is not a valid email!`
+          }
     },
     password : {
         type : String,
         required : true,
+        validate(value){
+            if(!npm_validator.isStrongPassword(value)){
+                throw  new Error("Password should be strong")
+            }
+        }
     },
     age : {
         type : Number,
@@ -39,14 +52,27 @@ const userSchema = new mongoose.Schema({
     photoUrl : {
         type : String,
         default : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiibOngFYog5Ri5UoFKH3CsHMOvomBLf4JAw&s',
+        validate(value){
+            if(!npm_validator.isURL(value,{ protocols: ['http','https','ftp'] }))
+            {
+                throw new Error("PhotoUrl should be a valid url")
+            }
+        }
     },
     about : {
         type : String,
         default : "Write about yourself here!"
     },
-    skills : {
-        type : [String]
+    skills: {
+        type: [String],
+        validate: {
+            validator: function(value) {
+                return value.length <= 20;
+            },
+            message: 'Too many skills! Maximum allowed is 20.'
+        }
     }
+
 },{timestamps : true})
 
 const User = mongoose.model('User', userSchema);
